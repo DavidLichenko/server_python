0597ef3df43b6db32da215e493bc6bd328e1d7a3
DavidLichenko <89989857+DavidLichenko@users.noreply.github.com>
2025-06-18 19:13:58 +0200

    initial commit3

--- main.py
+++ main.py
@@ -1,40 +1,28 @@
 import pandas as pd
 import yfinance as yf
-from mangum import Mangum
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from typing import List
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins, or specify a list of origins
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all HTTP methods
-    allow_headers=["*"],  # Allow all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
-
-handler = Mangum(app)
-
-
 
 @app.get("/")
 async def main():
-    return {"status":"success","data":"hello"}
+    return {"status": "success", "data": "hello"}
 
-
-
-# Function to fetch and format candlestick data for lightweight chart
+# Function to fetch and format candlestick data
 async def get_candlestick_data(symbol: str, timeframe: str = "1m", duration: str = "1d"):
-    # Fetch data
     ticker = yf.Ticker(symbol)
-    historical_data = ticker.history(period="1d", interval='1m')
-
-    # Reset the index to make 'Datetime' a column
+    historical_data = ticker.history(period=duration, interval=timeframe)
     historical_data.reset_index(inplace=True)
 
-    # Restructure data to match the desired JSON format
     formatted_data = historical_data.rename(columns={
         'Datetime': 'time',
         'Open': 'open',
@@ -43,13 +31,9 @@ async def get_candlestick_data(symbol: str, timeframe: str = "1m", duration: str
         'Close': 'close'
     })[['time', 'open', 'high', 'low', 'close']]
 
-    # Convert to dictionary with records orientation
-    json_data = formatted_data.to_dict(orient='records')
-
-    return json_data
+    return formatted_data.to_dict(orient='records')
 
 @app.get("/api/stocks/{symbol}/candlesticks/")
 async def get_historical_candlesticks(symbol: str, timeframe: str = "1m", duration: str = "1d"):
-    # Get the formatted candlestick data
     data = await get_candlestick_data(symbol, timeframe, duration)
-    return {"status":"success","data":data}
+    return {"status": "success", "data": data}
